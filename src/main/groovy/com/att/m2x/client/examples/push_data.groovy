c0a8043bf9e2d993d2872402df8cfa07995e20de
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2014-07-15 08:45:04 +0800

    Add code comments

--- src/main/groovy/com/att/m2x/client/examples/push_data.groovy
+++ src/main/groovy/com/att/m2x/client/examples/push_data.groovy
@@ -10,13 +10,19 @@ import com.att.m2x.client.M2XClient
 import com.att.m2x.client.api.stream.Value
 import com.att.m2x.client.api.stream.ValueListResponse
 
+// Your M2X key
 String key = "<key>"
+// The feed to push data
 String feedId = "<feed id>"
+// The steam to push data
 String streamName = "<stream name>"
+// The actual data
 String pushValue = "<value to push>"
 
 M2XClient client = new M2XClient(key)
 
+// This method is used to print values in a stream, we use it to
+// show the differences after pushing data
 def print_response(client, feedId, streamName) {
     ValueListResponse response = client.feed(feedId).streams().values(streamName)
     println "Values for feed: " + feedId + " stream: " + streamName
@@ -29,9 +35,13 @@ def print_response(client, feedId, streamName) {
 }
 
 println "Before pushing data:"
+// Print values before pushing the data
 print_response(client, feedId, streamName)
 
+// Push data to the stream, please refer to the Java library documentation
+// for details
 client.feed(feedId).streams().addValues(streamName, new Value(pushValue, new Date()))
 
 println "After pushing data:"
+// Print values after pushing the data
 print_response(client, feedId, streamName)
